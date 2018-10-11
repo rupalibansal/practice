b9c3eb0df31e80864ea17655b1cf662441c9363a
Rupali <r.rupali211@gmail.com>
2018-10-11 16:25:28 +1100

    Subject line (try to keep under 50 characters)
    Multi-line description of commit,
    feel free to be detailed.
    
    [Ticket: X]

--- specs/Assertions_Practice.js
+++ specs/Assertions_Practice.js
@@ -1,7 +1,7 @@
 // JASMINE: jasmine is a behaviour driven development (BDD) framework for testing the javascript code:
 // BDD means the test cases are written in easy understandable language 
 //Expectations are built with the function expect which takes a value, called the actual. It is chained with a Matcher function, which takes the expected value.
-
+// Assertions are just the validations .
 
 describe('Assertions Suite', () => {
 
